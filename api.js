3158238db95244c3a8a4ad3ec79fc798e4093f71
lareniar <43707260+lareniar@users.noreply.github.com>
2020-02-04 21:28:02 +0100

    Add files via upload

--- api.js
+++ api.js
@@ -1,9 +1,4 @@
-const port = process.argv[2] || 7000 //puerta
- 
-console.log('MYDBHOST:   '+process.env.MYDBHOST)
-console.log('MYUSER:     '+process.env.MYUSER)
-console.log('MYPASSWORD: '+process.env.MYPASSWORD)
- // Para correrlo
+// Para correrlo
 // MYDBHOST="localhost" MYDATABASE="mydb" MYUSER="root" MYPASSWORD="" node appapi.js
 const mysql = require('mysql');
 const express = require('express');
@@ -12,11 +7,16 @@ var bodyParser = require('body-parser')
 const app = express();
  
 const api = '/api/v1' //la ruta de la api
-const apiusers = api + '/users' //la ruta para ver los usuarios de la api
-
+const apiusers = api + '/Users' //la ruta para ver los usuarios de la api
+const port = process.argv[2] || 7000 //puerta
+ 
+console.log('MYDBHOST:   '+process.env.MYDBHOST)
+console.log('MYUSER:     '+process.env.MYUSER)
+console.log('MYPASSWORD: '+process.env.MYPASSWORD)
+console.log('MYDATABASE: '+process.env.MYDATABASE)
  
 var conn = mysql.createConnection({
-  host: process.env.MYDBHOST || "localhost",
+  host: process.env.MYDBHOST || "db",
   user: process.env.MYUSER || "root",
   password: process.env.MYPASSWORD || "root",
   database: process.env.MYDATABASE || "laravel"
@@ -68,6 +68,64 @@ app.get( apiusers, (req, res) => {
     res.send(result)
   });
 })
+
+
+// SELECT ID
+app.get(apiusers + '/:id', (req, res) => {
+  console.log('get ' + req.params.id)
+  var sql = 'SELECT Id _id, Nombre, Apellido FROM usuarios WHERE Id = ?';
+  conn.query(sql, [parseInt(req.params.id)],  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send(result)
+  });
+})
+
+// // SELECT ALL
+// app.get( apiusers, (req, res) => {
+//   console.log('get all')
+//   var sql = 'SELECT id _id, name, email FROM users';
+//   conn.query(sql,  (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     res.send(result)
+//   });
+// })
+
+// INSERT
+app.post( apiusers, (req, res) => {
+  console.log('post ');
+  console.log(req.body);
+
+  var sql = 'INSERT INTO Users (Nombre, Apellido) VALUES(?,?)';
+  conn.query(sql, [req.body.name, req.body.email],  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send(result)
+  });
+})
+
+// UPDATE
+app.put( apiusers, (req, res) => {
+  console.log('put ' + req.body._id)
+  var sql = 'UPDATE Users SET Nombre = ?, Apellido = ? WHERE Id = ?';
+  conn.query(sql, [req.body.name, req.body.email, req.body._id],  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send(result)
+  });
+})
+
+// DELETE 
+app.delete( apiusers + '/:id', (req, res) => {
+  console.log('delete ' + req.params.id)
+  var sql = 'DELETE FROM Users WHERE Id = ?';
+  conn.query(sql, [parseInt(req.params.id)],  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send(result)
+  });
+})
  
 // ======  TODAS LAS DEMAS RUTAS =====
 // 404 Siempre al final
@@ -80,4 +138,3 @@ app.get('*', (req, res) => {
     timestamp: (new Date()).toUTCString()
   })
 })
-
