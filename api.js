a67daad88d36a6b32a61c58aee488bfec42aba64
lareniar <43707260+lareniar@users.noreply.github.com>
2020-02-05 09:31:28 +0100

    Add files via upload

--- api.js
+++ api.js
@@ -3,28 +3,28 @@
 const mysql = require('mysql');
 const express = require('express');
 var bodyParser = require('body-parser')
- 
+
 const app = express();
- 
-const api = '/api/v1' //la ruta de la api
-const apiusers = api + '/Users' //la ruta para ver los usuarios de la api
-const port = process.argv[2] || 7000 //puerta
- 
+
+const api = '/api/v1'
+const apiusers = api + '/users'
+const port = process.argv[2] || 7000
+
 console.log('MYDBHOST:   '+process.env.MYDBHOST)
 console.log('MYUSER:     '+process.env.MYUSER)
 console.log('MYPASSWORD: '+process.env.MYPASSWORD)
 console.log('MYDATABASE: '+process.env.MYDATABASE)
- 
+
 var conn = mysql.createConnection({
   host: process.env.MYDBHOST || "db",
   user: process.env.MYUSER || "root",
   password: process.env.MYPASSWORD || "root",
   database: process.env.MYDATABASE || "laravel"
 });
- 
+
 // la bd y el server
 var server
- 
+
 conn.connect(err => {
   if (err)  {
     console.error(err)
@@ -37,7 +37,7 @@ conn.connect(err => {
     console.log('webapi listening on ' + server.address().port)
   })
 })
- 
+
 //CORS
 app.use( (req, res, next) =>{
   res.header('Access-Control-Allow-Origin', "*");
@@ -46,22 +46,40 @@ app.use( (req, res, next) =>{
   next();
 })
 app.use(bodyParser.json());
- 
+
 //====
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.status(200).send({
     success: "true",
     status: 200,
     data: {},
     timestamp: (new Date()).toUTCString()
   })
-})
- 
+})*/
+
 // =========  API ===========
+// SELECT ID
+app.get('/', (req, res) => {
+  var sql = 'SELECT Id,Nombre,Apellido,Edad FROM Personas';
+  conn.query(sql, [parseInt(req.params.id)],  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send(result)
+  });
+})
+app.get('/borrar/:id', (req, res) => {
+  console.log('delete ' + req.params.id)
+  var sql = 'DELETE FROM Personas WHERE id = ?';
+  conn.query(sql, [parseInt(req.params.id)],  (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send(result)
+  });
+})
 // SELECT ALL
 app.get( apiusers, (req, res) => {
   console.log('get all')
-  var sql = 'SELECT * FROM usuarios';
+  var sql = 'SELECT Id,Nombre,Apellido,Edad FROM users';
   conn.query(sql,  (err, result) => {
     if (err) throw err;
     console.log(result);
@@ -69,35 +87,12 @@ app.get( apiusers, (req, res) => {
   });
 })
 
-
-// SELECT ID
-app.get(apiusers + '/:id', (req, res) => {
-  console.log('get ' + req.params.id)
-  var sql = 'SELECT Id _id, Nombre, Apellido FROM usuarios WHERE Id = ?';
-  conn.query(sql, [parseInt(req.params.id)],  (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.send(result)
-  });
-})
-
-// // SELECT ALL
-// app.get( apiusers, (req, res) => {
-//   console.log('get all')
-//   var sql = 'SELECT id _id, name, email FROM users';
-//   conn.query(sql,  (err, result) => {
-//     if (err) throw err;
-//     console.log(result);
-//     res.send(result)
-//   });
-// })
-
 // INSERT
 app.post( apiusers, (req, res) => {
   console.log('post ');
   console.log(req.body);
 
-  var sql = 'INSERT INTO Users (Nombre, Apellido) VALUES(?,?)';
+  var sql = 'INSERT INTO users (name, email) VALUES(?,?)';
   conn.query(sql, [req.body.name, req.body.email],  (err, result) => {
     if (err) throw err;
     console.log(result);
@@ -108,7 +103,7 @@ app.post( apiusers, (req, res) => {
 // UPDATE
 app.put( apiusers, (req, res) => {
   console.log('put ' + req.body._id)
-  var sql = 'UPDATE Users SET Nombre = ?, Apellido = ? WHERE Id = ?';
+  var sql = 'UPDATE users SET name = ?, email = ? WHERE id = ?';
   conn.query(sql, [req.body.name, req.body.email, req.body._id],  (err, result) => {
     if (err) throw err;
     console.log(result);
@@ -117,16 +112,8 @@ app.put( apiusers, (req, res) => {
 })
 
 // DELETE 
-app.delete( apiusers + '/:id', (req, res) => {
-  console.log('delete ' + req.params.id)
-  var sql = 'DELETE FROM Users WHERE Id = ?';
-  conn.query(sql, [parseInt(req.params.id)],  (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.send(result)
-  });
-})
- 
+
+
 // ======  TODAS LAS DEMAS RUTAS =====
 // 404 Siempre al final
 app.get('*', (req, res) => {
